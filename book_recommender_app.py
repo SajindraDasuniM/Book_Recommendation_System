c71bbbc3fc92c264f89cf192fd4cbf6d91bcba19
SajindraDasuniM <159102170+SajindraDasuniM@users.noreply.github.com>
2026-03-01 12:26:47 +1000

    Comment out ISBN display in recommendations
    Comment out the line displaying book ISBN.

--- book_recommender_app.py
+++ book_recommender_app.py
@@ -84,7 +84,7 @@ if st.button("Recommend"):
         st.subheader("✨ Recommended Books")
 
     for book in recommendations:
-        st.write("### 📘", book['ISBN'])
+        #st.write("### 📘", book['ISBN'])
         st.write("### 📘", book['title'])
         st.write("### 📘", book['authors'])
         st.write("### 📘", book['Subject'])
